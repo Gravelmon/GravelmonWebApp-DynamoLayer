53aec0515ac6d90a7b0104bdd75e16c927753805
StijnArts <94121799+StijnArts@users.noreply.github.com>
2026-05-05 19:51:00 +0200

    setup for layer

--- lib/main.ts
+++ lib/main.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register'
 import * as cdk from 'aws-cdk-lib'
+// @ts-ignore
 import {GravelmonDynamoDBLayerStack} from "./stacks/GravelmonDynamoDBLayerStack";
 
 const devEnv = {
